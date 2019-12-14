01ddb2aee0b10c5de70e2e9f6ecc2444e5d9f287
Cloud-66 <cloudoblakorain@yandex.ru>
2019-12-14 13:18:56 +0300

    complete hello world lesson

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,14 +11,27 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to peaple", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("", func(t *testing.T) {
+		got := Hello("Elodie", french)
+		want := "Bonjure, Elodie"
 
 		assertCorrectMessage(t, got, want)
 	})
