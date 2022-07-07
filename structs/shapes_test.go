75ff133d2ea36ca2c181eab02e682a78853f6a07
Cloud-66 <cloud-66@ya.ru>
2022-07-07 19:34:50 +0300

    structs refactor

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -16,25 +16,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{
 			Width:  12.0,
 			Height: 6.0,
 		}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, 72.0)
 	})
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2g want %.2g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
