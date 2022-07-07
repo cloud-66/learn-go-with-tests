26aae090dbc96bafe5a0e8ac5c8e1b77f19066f5
Cloud-66 <cloud-66@ya.ru>
2022-07-07 21:51:06 +0300

    structs refactor

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -20,11 +20,11 @@ func TestArea(t *testing.T) {
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
-
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
 	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{
 			Width:  12.0,
@@ -32,8 +32,32 @@ func TestArea(t *testing.T) {
 		}
 		checkArea(t, rectangle, 72.0)
 	})
+
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
 		checkArea(t, circle, 314.1592653589793)
 	})
+}
+
+func TestTableArea(t *testing.T) {
+
+	areaTest := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 7.0},
+		{name: "Shape", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTest {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
 }
