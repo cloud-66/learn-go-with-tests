9b4682ce3ceace78c7c43375cbcf748868048f7b
Cloud-66 <cloud-66@ya.ru>
2022-07-14 21:35:13 +0300

    dependency injection refactor

--- di/di.go
+++ di/di.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	Greet(os.Stdout, "Flodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
 
 func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
