453abd71a987b1fd956e0fa1045753ee73ee8c2c
Cloud-66 <cloud-66@ya.ru>
2022-07-18 21:05:45 +0300

    mocking refactor

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -24,15 +24,17 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct {
-}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type SpyCountdownOperations struct {
 	Calls []string
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
 }
 
 func (s *SpyCountdownOperations) Sleep() {
@@ -44,8 +46,16 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	return
 }
 
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
